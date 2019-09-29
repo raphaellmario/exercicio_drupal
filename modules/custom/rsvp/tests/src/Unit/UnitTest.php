ed0133b03f0b2412a77392804ff151dc5ed12b73
raphael <raphaelr@ciandt.com>
2019-09-29 16:45:31 -0300

    update

--- modules/custom/rsvp/tests/src/Unit/UnitTest.php
+++ modules/custom/rsvp/tests/src/Unit/UnitTest.php
@@ -2,22 +2,20 @@
 namespace Drupal\Tests\rsvp\Unit;
 use Drupal\Tests\UnitTestCase;
 use Drupal\rsvp\Unit;
+
 /**
- * Simple test to ensure that asserts pass.
- *
- * @group phpunit_example
+ * {@inheritdoc}
  */
 class UnitTest extends UnitTestCase {
   protected $unit;
   /**
-   * Before a test method is run, setUp() is invoked.
-   * Create new unit object.
+   * {@inheritdoc}
    */
   public function setUp() {
     $this->unit = new Unit();
   }
   /**
-   * @covers Drupal\phpunit_example\Unit::setLength
+   * {@inheritdoc}
    */
   public function testSetLength() {
     $this->assertEquals(0, $this->unit->getLength());
@@ -25,15 +23,14 @@ class UnitTest extends UnitTestCase {
     $this->assertEquals(9, $this->unit->getLength());
   }
   /**
-   * @covers Drupal\phpunit_example\Unit::getLength
+   * {@inheritdoc}
    */
   public function testGetLength() {
     $this->unit->setLength(9);
     $this->assertNotEquals(10, $this->unit->getLength());
   }
   /**
-   * Once test method has finished running, whether it succeeded or failed, tearDown() will be invoked.
-   * Unset the $unit object.
+   * {@inheritdoc}
    */
   public function tearDown() {
     unset($this->unit);
